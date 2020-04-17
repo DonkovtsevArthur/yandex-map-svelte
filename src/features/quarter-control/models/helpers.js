1a7be683a0b18ed671cf1acb7932ee61b98d2e27
Донковцев Артур <a.donkovtsev@zebrains.team>
2020-04-17 12:14:21 +0300

    добавил определение квартала по дефолту + выбора квартала по отдельности

---
+++ src/features/quarter-control/models/helpers.js
@@ -0,0 +1,19 @@
+const setSelectedValues = (state, newValue) => {
+  const isChecked = state.some(({ start }) => start === newValue.start);
+  const newState = isChecked
+    ? state.filter(({ start }) => start !== newValue.start)
+    : [...state, newValue];
+  return [...newState];
+};
+
+const setFilterData = ({ data, selectedValue }) => {
+  const newData = data.filter(({ day }) => {
+    return selectedValue.some(({ start, end }) => {
+      return day >= start && day <= end;
+    });
+  });
+
+  return newData.length ? newData : data;
+};
+
+export { setFilterData, setSelectedValues };
